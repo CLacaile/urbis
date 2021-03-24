4e4ca93f4ead4e7a025bcab34c71a96475d711ed
CLacaile@MacMini <clementlacaile@gmail.com>
2021-03-24 21:31:30 +0100

    add plane, orbit controls and background

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -1,20 +1,22 @@
-import * as THREE from 'three';
-import {ElementRef, Injectable, NgZone, OnDestroy} from '@angular/core';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { ElementRef, Injectable, NgZone, OnDestroy } from "@angular/core";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class EngineService implements OnDestroy {
   private canvas: HTMLCanvasElement;
   private renderer: THREE.WebGLRenderer;
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private light: THREE.AmbientLight;
+  private controls: OrbitControls;
+  private loader: THREE.TextureLoader;
 
   private cube: THREE.Mesh;
 
   private frameId: number = null;
 
-  public constructor(private ngZone: NgZone) {
-  }
+  public constructor(private ngZone: NgZone) {}
 
   public ngOnDestroy(): void {
     if (this.frameId != null) {
@@ -28,45 +30,80 @@ export class EngineService implements OnDestroy {
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
-      alpha: true,    // transparent background
-      antialias: true // smooth edges
+      alpha: true, // transparent background
+      antialias: true, // smooth edges
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // create the scene
+    // SCENE
     this.scene = new THREE.Scene();
 
+    // CAMERA
     this.camera = new THREE.PerspectiveCamera(
-      75, window.innerWidth / window.innerHeight, 0.1, 1000
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
     );
-    this.camera.position.z = 5;
     this.scene.add(this.camera);
 
-    // soft white light
+    // CONTROLS
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    // LOADER
+    this.loader = new THREE.TextureLoader();
+
+    // UPDATE CAMERA POSITION
+    this.camera.position.x = 0;
+    this.camera.position.y = 10;
+    this.camera.position.z = 30;
+    this.controls.update();
+
+    // SOFT WHITE LIGHT
     this.light = new THREE.AmbientLight(0x404040);
     this.light.position.z = 10;
     this.scene.add(this.light);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    this.cube = new THREE.Mesh(geometry, material);
+    // CUBE
+    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     this.scene.add(this.cube);
 
+    // PLANE
+    // height map
+    const heightMap = this.loader.load("/assets/height-map.png");
+    // texture map
+    const textureMap = this.loader.load("/assets/grass-texture.png");
+    // plane
+    const planeGeometry = new THREE.PlaneBufferGeometry(30, 30, 32, 32);
+    const planeMaterial = new THREE.MeshStandardMaterial({
+      color: 0x556b2f,
+      side: THREE.DoubleSide,
+      wireframe: true,
+      displacementMap: heightMap,
+      displacementScale: 3,
+      displacementBias: -3,
+      map: textureMap,
+    });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = Math.PI / 2;
+    this.scene.add(plane);
   }
 
   public animate(): void {
     // We have to run this outside angular zones,
     // because it could trigger heavy changeDetection cycles.
     this.ngZone.runOutsideAngular(() => {
-      if (document.readyState !== 'loading') {
+      if (document.readyState !== "loading") {
         this.render();
       } else {
-        window.addEventListener('DOMContentLoaded', () => {
+        window.addEventListener("DOMContentLoaded", () => {
           this.render();
         });
       }
 
-      window.addEventListener('resize', () => {
+      window.addEventListener("resize", () => {
         this.resize();
       });
     });
@@ -77,8 +114,9 @@ export class EngineService implements OnDestroy {
       this.render();
     });
 
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    //this.cube.rotation.x += 0.01;
+    //this.cube.rotation.y += 0.01;
+    this.controls.update();
     this.renderer.render(this.scene, this.camera);
   }
 
